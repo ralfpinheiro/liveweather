caed665273f52ab7a8c57d1a11f02e30e05d467e
Ralf Pinheiro <pinheiro.ralf@gmail.com>
2019-12-14 16:41:05 -0800

    {

--- src/App.js
+++ src/App.js
@@ -5,10 +5,15 @@ import Weather from './Weather'
 
 const APIKEY = 'a371ff5e548780bb173b785f54f58a72';
 
+const city1 = 6173331; // sjbvista
+const city2 = 3448902; // vancouver
+const app1 = document.querySelector('#temperature1');
+const app2 = document.querySelector('#temperature2');
+
 function App() {
   const [weather, setWeather] = useState([]);
 
-  async function fetchData(e) {
+  {/* async function fetchData(e) {
     e.preventDefault()
     const city = e.target.elements.city.value;
     const country = e.target.elements.country.value;
@@ -16,14 +21,40 @@ function App() {
     await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city},${country}&APPID=${APIKEY}`)
     .then(res => res.json())
     .then(data => data)
+    setWeather ({
+    data: apiData,
+    city: apiData.name,
+    country: apiData.sys.country,
+    temperature: apiData.main.temp,
+    description: apiData.weather[0].description,
+    error: ''
     )
-  }
+  } */}
+
+  const getTemp = fetch(`https://api.openweathermap.org/data/2.5/group?id=${city1},${city2}&units=metric&APPID=${APIKEY}`).then(function (response) {
+    // The API call was successful!
+    return response.json();
+  }).then(function (data) {
+    // This is the JSON from our response
+    const temp1 = data.list[0].main.temp;
+    const temp2 = data.list[1].main.temp;                                                                                                                                                                    
+    const template1 = `<span>${temp1.toFixed(0)} Degrees in Vancouver right now!</span>`;  
+    const template2 = `<span>${temp2.toFixed(0)} Degrees in Sao Joao right now!</span>`;
+    
+    console.warn(`${temp1.toFixed(0)} 'Degrees in Vancouver right now!`);
+
+  }).catch(function (err) {
+    // There was an error
+    console.warn('Something went wrong.', err);
+  });
+
+
 
   const appStyle = {
     container : {
       display : 'flex',
-      'align-items': 'center',
-      'justify-content': 'center',
+      alignItems: 'center',
+      justifyContent: 'center',
       height: '100vh',
       background: '#f9f9f9'
     }
@@ -33,12 +64,22 @@ function App() {
   return (
     <div className="App" style={ appStyle.container }>
       <div>
-          <h3 style={{ 'font-size': '34px'}} >LiveWeather</h3>
-          <Form getWeather={fetchData} />
-          {console.log(weather)}
+          <h3 style={{ fontSize: '34px'}} >LiveWeather</h3>
+          <div id=""></div>
+          <div id="temperature1"></div>
+          <div id="temperature2"></div>
+          {/*<Form  getWeather={fetchData}  />*/}
+          <Weather 
+          city={weather.city}
+          country={weather.country}
+          temperature={weather.temperature}
+          description={weather.description}
+          error={weather.e}
+          />
         </div>
     </div>
-  );
-}
+    );
+  }
+
 
 export default App;
--- src/Weather.js
+++ src/Weather.js
@@ -1,9 +1,14 @@
 import React from 'react';
 
-const Weather = (props) => {
+const Weather = ({ getWeather, city, country, temperature, description, error }) => {
+    
     return (
         <div>
-
+            
+            {city && country && <p>{city}, " "{country}</p>}
+            {temperature && <p>{temperature}</p>}
+            {description && <p>{description}</p>}
+            {error && <p>{error}</p>}
         </div>
     )
 }
